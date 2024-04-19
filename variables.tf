f26ac3908cc72b52fba03294427507d3d3718137
jedimickryan <110208954+jedimickryan@users.noreply.github.com>
2024-04-19 13:32:13 +1000

    update

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  type        = string
-  default     = "us-west-2"
-}
-
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
