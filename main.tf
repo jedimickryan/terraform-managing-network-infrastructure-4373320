fdba453871a1162be8964bd672f87c99936012a5
jedimickryan <110208954+jedimickryan@users.noreply.github.com>
2024-04-19 13:27:09 +1000

    network test server

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "Network Test Server"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   type        = string
   default     = "us-west-2"
 }
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
