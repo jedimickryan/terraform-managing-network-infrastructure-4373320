9cd6d530cf3733ebd2e0dca960d987c0217928da
jedimickryan <110208954+jedimickryan@users.noreply.github.com>
2024-04-22 14:23:43 +1000

    ec2 builds

--- variables.tf
+++ variables.tf
@@ -1,7 +1,27 @@
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
+  type        = string
   default     = "t3.nano"
 }
+
+variable "instances" {
+  description = "List of AWS instances"
+  type        = list(string)
+  default     = ["ec2-public", "ec2-private"]
+}
+
+variable "ami_id" {
+  description = "ID of amazone machine image"
+  type        = string
+  default     = "ami-0f0ba639982a32edb"
+}
+variable "public_key" {
+  description = "Path to public key"
+  type        = string
+  sensitive   = true
+  default     = "keys/ec2.pub"
+}
+
 variable "region" {
   type    = string
   default = "us-west-2"
@@ -24,9 +44,3 @@ variable "subnets" {
   type        = list(string)
   default     = ["sub-public", "sub-private"]
 }
-
-#
-#variable "subnet_cidr" {
-#  description  = "CIDR of AWS subnet"
-#  type         = string
-#}
