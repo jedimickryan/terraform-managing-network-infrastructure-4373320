24e6954033fe2a40f956f72bb96f96c7bd86f4e2
jedimickryan <110208954+jedimickryan@users.noreply.github.com>
2024-04-22 13:44:07 +1000

    build multiple subnets

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "test" {
 
 # Build VPC for Development
 
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "dev" {
   cidr_block = var.vpc_cidr
 
   tags = {
@@ -33,21 +33,22 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
-resource "aws_subnet" "dev-subnet-01" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = var.subnets[0]
+resource "aws_subnet" "dev" {
+  count  = length(var.subnets) # count = length function of variable subnets
+  vpc_id = aws_vpc.dev.id
 
+  cidr_block = cidrsubnet(aws_vpc.dev.cidr_block, 4, count.index + 1)
   tags = {
-    Name = "DEV Subnet 01"
+    Name = var.subnets[count.index]
   }
 }
 
-resource "aws_subnet" "dev-subnet-02" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = var.subnets[1]
-
-
-  tags = {
-    Name = "DEV Subnet 02"
-  }
-}
+#resource "aws_subnet" "dev-subnet-02" {
+#  vpc_id     = aws_vpc.dev-vpc.id
+#  cidr_block = var.subnets[1]
+#
+#
+#  tags = {
+#    Name = "DEV Subnet 02"
+#  }
+#}
--- variables.tf
+++ variables.tf
@@ -9,18 +9,17 @@ variable "region" {
 
 variable "vpc_name" {
   description = "Name of AWS VPC"
-  default     = "Dev-VPC"
+  type        = string
 }
 
 variable "vpc_cidr" {
   description = "CIDR of AWS VPC"
-  default     = "10.0.0.0/16"
+  type    = string
 }
 
 variable "subnets" {
-  description = "list of AWS subnets"
+  description = "Name of AWS subnets"
   type        = list(string)
-  default     = ["10.0.1.0/32", "10.0.2.0/32"]
 }
 
 #
